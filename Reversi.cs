0248615a727d523695749941acd618ba86ba98bc
3TKY <bastiaantk@hotmail.com>
2014-10-10 21:18:21 +0200

    Schermgrootte
    Het scherm wordt groot genoeg om alles elementen te laten zien

--- Reversi.cs
+++ Reversi.cs
@@ -11,13 +11,14 @@ public class Scherm : Form
     Tekst speler1Score = new Tekst("Speler 1: 0", new Point(45, 110));
     Tekst speler2Score = new Tekst ("Speler 2: 0",new Point(45, 140));
     Tekst beurten = new Tekst ("Beurt: ", new Point(45, 170));
-    const int veldBreedte = 5;
-    const int veldHoogte = 5;
+    const int veldBreedte = 10;
+    const int veldHoogte = 10;
     Veld speelVeld = new Veld(new Point(180, 20), veldHoogte, veldBreedte);
 
     public Scherm() 
     {
-        this.ClientSize = new Size(200 + speelVeld.vakGrootte * veldBreedte, 40 + speelVeld.vakGrootte * veldHoogte);
+        this.ClientSize = new Size(200 + speelVeld.vakGrootte * veldBreedte, 40 + speelVeld.vakGrootte * speelVeld.veldHoogte);
+        if (this.ClientSize.Height < 210) this.ClientSize = new Size(200 + speelVeld.vakGrootte * speelVeld.veldBreedte, 210);
 
         this.Text = "Reversi";
         this.Paint += tekenStenen;
