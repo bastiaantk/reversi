eaeafbbdb463c3a02ddd6e5d60ceae3dcc505c2b
3TKY <bastiaantk@hotmail.com>
2014-10-16 00:21:21 +0200

    Veldgrootte optie
    Optie om de grootte van het speelveld aan te kunnen passen

--- Reversi.cs
+++ Reversi.cs
@@ -19,8 +19,8 @@ public class Scherm : Form
 
     Tekst beurten = new Tekst ("Beurt: ", new Point(50, 230));
 
-    const int veldBreedte = 7;
-    const int veldHoogte = 6;
+    public int VeldBreedte = 6;
+    public int VeldHoogte = 6;
 
     Veld speelVeld;
 
@@ -28,14 +28,14 @@ public class Scherm : Form
 
     public Scherm() 
     {
-        this.speelVeld = new Veld(new Point(180, 20), veldHoogte, veldBreedte, this.SpelerNamen);
+        //Scherm dat de namen van de spelers vraag wordt aangeroepen
+        this.optieScherm = new OptieScherm(this);
+        this.optieScherm.ShowDialog(); 
+
+        this.speelVeld = new Veld(new Point(180, 20), VeldHoogte, VeldBreedte, this.SpelerNamen);
         this.StartPosition = FormStartPosition.CenterScreen;
 
-        //Schermgrootte wordt aangepast aan de grootte van het speelbord
-        this.ClientSize = new Size(200 + speelVeld.vakGrootte * speelVeld.veldBreedte, 40 + speelVeld.vakGrootte * speelVeld.veldHoogte);
-        if (this.ClientSize.Height < 210) this.ClientSize = new Size(200 + speelVeld.vakGrootte * speelVeld.veldBreedte, 280);
-        this.MaximumSize = this.Size;
-        this.MinimumSize = this.Size;
+        this.schermGrootte();
 
         this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
         this.Paint += tekenStenen;
@@ -58,10 +58,22 @@ public class Scherm : Form
 
         this.speelVeld.Paint += this.updateScherm;
         this.Controls.Add(this.speelVeld);
+    }
 
-        //Scherm dat de namen van de spelers vraag wordt aangeroepen
-        this.optieScherm = new OptieScherm(this);
-        this.optieScherm.ShowDialog();   
+    private void schermGrootte()
+    {
+        //Reset maximum grootte, zodat de grootte daarbuiten kan worden aangepast
+        this.MaximumSize = new Size();
+
+        //Grootte van het scherm wordt bepaald door het aantal vakjes in het speelveld
+        this.ClientSize = new Size(200 + this.speelVeld.vakGrootte * this.speelVeld.veldBreedte, 40 + this.speelVeld.vakGrootte * this.speelVeld.veldHoogte);
+        if (this.ClientSize.Height < 210) this.ClientSize = new Size(200 + this.speelVeld.vakGrootte * this.speelVeld.veldBreedte, 280);
+        
+        //Stel maximum grootte weer in op de huidige grootte, zodat er niet geresized kan worden
+        this.MaximumSize = this.Size;
+
+        //Plaats scherm weer in het midden
+        this.CenterToScreen();
     }
 
     //Methode om de stenen te tekenen die de kleuren van beide spelers in de UI tonen
@@ -70,27 +82,36 @@ public class Scherm : Form
         Graphics gr = pea.Graphics;
         gr.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
 
+        //Speler pictogrammen
         gr.FillEllipse(this.speelVeld.Speler1Brush, 20, 114, 20, 20);
         gr.FillEllipse(this.speelVeld.Speler2Brush, 20, 174, 20, 20);
         
+        //Beurt pictogram
         if (this.speelVeld.Beurt % 2 == 0) gr.FillEllipse(this.speelVeld.Speler1Brush, 20, 234, 20, 20);
         else if (this.speelVeld.Beurt % 2 == 1) gr.FillEllipse(this.speelVeld.Speler2Brush, 20, 234, 20, 20);
 
+        //Cirkels om de pictogrammen voor betere zichtbaarheid
         gr.DrawEllipse(Pens.Black, 20, 114, 20, 20);
         gr.DrawEllipse(Pens.Black, 20, 174, 20, 20);
-        gr.DrawEllipse(Pens.Black, 20, 114, 20, 20);
         gr.DrawEllipse(Pens.Black, 20, 234, 20, 20);
     }
 
     private void klik(object sender, EventArgs e)
     {
+        //Start een nieuw spel klik
         if (sender == this.nieuwSpel)
         {
             this.optieScherm.ShowDialog();
-            this.speelVeld.InitVeld();
-            this.speelVeld.StartSpel();
-            this.speelVeld.Invalidate();
+
+            this.Controls.Remove(this.speelVeld);
+
+            this.speelVeld = new Veld(new Point(180, 20), VeldHoogte, VeldBreedte, this.SpelerNamen);
+            this.speelVeld.Paint += this.updateScherm;
+            this.Controls.Add(speelVeld);
+
+            this.schermGrootte();
         }
+        //Geeft hints weer klik
         else if (sender == this.hint)
         {
             this.speelVeld.Hint = true;
@@ -103,12 +124,20 @@ public class Scherm : Form
     {
         this.speler1Naam.Text = SpelerNamen[0];
         this.speler2Naam.Text = SpelerNamen[1];
+
         this.speler1Score.Text = "Stenen: " + Convert.ToString(this.speelVeld.Score(1));
         this.speler2Score.Text = "Stenen: " + Convert.ToString(this.speelVeld.Score(2));
+
         this.beurten.Text = "Beurten: " + Convert.ToString(this.speelVeld.Beurt + 1);
-        this.Text = "Reversi - " + this.speelVeld.Status;
+
+        if (this.speelVeld.Status != "") this.Text = "Reversi - " + this.speelVeld.Status;
+        else this.Text = "Reversi";
+
+        //Knop voor hints kan niet worden gebruikt als hints niet zijn toegestaan
         if (this.speelVeld.Gestart && this.HintsToegestaan) this.hint.Enabled = true;
         else this.hint.Enabled = false;
+
+        //Opnieuw tekenen zodat o.a. het beurt pictogram wordt aangepast
         this.Invalidate();
     }
 }
--- optieScherm.cs
+++ optieScherm.cs
@@ -5,49 +5,112 @@ using System.Windows.Forms;
 //Scherm voor het invoeren van spelernamen
 public class OptieScherm : Form
 {
-    public Invoer naam1 = new Invoer("", new Point(40, 55));
-    public Invoer naam2 = new Invoer("", new Point(40, 105));
-    public CheckBox hints = new CheckBox();
+    public Invoer Naam1 = new Invoer("", new Point(40, 55));
+    public Invoer Naam2 = new Invoer("", new Point(40, 105));
+    
+    public Invoer Hoogte = new Invoer("6", new Point(200, 125));
+    public Invoer Breedte = new Invoer("6", new Point(200, 55));
+
+    public CheckBox Hints = new CheckBox();
+
+    Knop optieKnop = new Knop("Spelen", new Point(40, 190));
+    Knop resetKnop = new Knop("Reset", new Point(200, 190));
+
     Scherm spelScherm;
 
     public OptieScherm(Scherm spelScherm)
     {
         this.spelScherm = spelScherm;
+        Naam1.TabIndex = 0;
+        Naam2.TabIndex = 1;
+        Hoogte.TabIndex = 2;
+        Breedte.TabIndex = 3;
+        Hints.TabIndex = 4;
 
         this.Text = "Reversi - Opties";
         this.StartPosition = FormStartPosition.CenterScreen;
-        this.ClientSize = new Size(220, 255);
+        this.ClientSize = new Size(330, 255);
         this.MaximumSize = this.Size;
         this.MinimumSize = this.Size;
         this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
 
         this.Controls.Add(new Tekst("Spelers:", new Point(40, 20)));
 
-        this.naam1.Text = spelScherm.SpelerNamen[0];
-        this.naam1.MaxLength = 10;
-        this.Controls.Add(this.naam1);
+        this.Naam1.Text = spelScherm.SpelerNamen[0];
+        this.Naam1.MaxLength = 10;
+        this.Controls.Add(this.Naam1);
 
-        this.naam2.Text = spelScherm.SpelerNamen[1];
-        this.naam2.MaxLength = 10;
-        this.Controls.Add(this.naam2);
+        this.Naam2.Text = spelScherm.SpelerNamen[1];
+        this.Naam2.MaxLength = 10;
+        this.Controls.Add(this.Naam2);
 
-        this.hints.Text = "Hints";
-        this.hints.Font = new Font("Corbel", 14);
-        this.hints.Checked = true;
-        this.hints.Location = new Point(40, 155);
-        this.Controls.Add(this.hints);
+        this.Controls.Add(new Tekst("Hoogte:", new Point(200, 20)));
+        this.Hoogte.MaxLength = 2;
+        this.Hoogte.Width = 90;
+        this.Controls.Add(this.Hoogte);
 
-        Knop naamKnop = new Knop("Spelen", new Point(40, 190));
-        naamKnop.Click += this.opslaan;
-        this.Controls.Add(naamKnop);
+        this.Controls.Add(new Tekst("Breedte:", new Point(200, 90)));
+        this.Breedte.MaxLength = 2;
+        this.Breedte.Width = 90;
+        this.Controls.Add(this.Breedte);
+
+        this.Hints.Text = "Hints";
+        this.Hints.Font = new Font("Corbel", 14);
+        this.Hints.Checked = true;
+        this.Hints.Location = new Point(40, 155);
+        this.Controls.Add(this.Hints);
+
+        
+        this.optieKnop.Click += this.klik;
+        this.Controls.Add(this.optieKnop);
+
+        this.resetKnop.Width = 90;
+        this.resetKnop.Click += this.klik;
+        this.Controls.Add(this.resetKnop);
     }
 
-    private void opslaan(object obj, EventArgs e)
+    private void klik(object sender, EventArgs e)
     {
-        //Opties worden voor het spelScherm ingesteld
-        this.spelScherm.HintsToegestaan = this.hints.Checked;
-        this.spelScherm.SpelerNamen[0] = this.naam1.Text;
-        this.spelScherm.SpelerNamen[1] = this.naam2.Text;
-        this.Close();
+        if (sender == this.optieKnop)
+        {
+            //Opties worden voor het spelScherm ingesteld
+            this.spelScherm.HintsToegestaan = this.Hints.Checked;
+            this.spelScherm.SpelerNamen[0] = this.Naam1.Text;
+            this.spelScherm.SpelerNamen[1] = this.Naam2.Text;
+
+            int veldHoogte;
+            int veldBreedte;
+            try
+            {
+                veldHoogte = Convert.ToInt32(this.Hoogte.Text);
+            }
+            catch
+            {
+                veldHoogte = 6;
+            }
+
+            try
+            {
+                veldBreedte = Convert.ToInt32(this.Breedte.Text);
+            }
+            catch
+            {
+                veldBreedte = 6;
+            }
+
+            this.spelScherm.VeldHoogte = veldHoogte;
+            this.spelScherm.VeldBreedte = veldBreedte;
+
+            this.Close();
+        }
+
+        if (sender == this.resetKnop)
+        {
+            this.Naam1.Text = "Speler 1";
+            this.Naam2.Text = "Speler 2";
+            this.Breedte.Text = "6";
+            this.Hoogte.Text = "6";
+            this.Hints.Checked = true;
+        }
     }
 }
